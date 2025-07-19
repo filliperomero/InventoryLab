a4e5ae1b9006cc26b82c67cd994f3b1733cbd9b9
filliperomero <fillipe.romero@gmail.com>
2025-07-19 00:33:16 -0600

    Add Item PopUp Class

---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
@@ -0,0 +1,77 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Editor/VREditor/Private/UI/VREditorBaseUserWidget.h"
+#include "Inv_ItemPopUp.generated.h"
+
+class USizeBox;
+class UTextBlock;
+class USlider;
+class UButton;
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FPopUpMenuSplit, int32, SplitAmount, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuDrop, int32, Index);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPopUpMenuConsume, int32, Index);
+
+/**
+ * The item popup widget shows up when right-clicking on an item in the inventory grid.
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_ItemPopUp : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnInitialized() override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+
+	FPopUpMenuSplit OnSplit;
+	FPopUpMenuDrop OnDrop;
+	FPopUpMenuConsume OnConsume;
+
+	void CollapseSplitButton() const;
+	void CollapseConsumeButton() const;
+	void SetSliderParams(const float Max, const float Value) const;
+	FVector2D GetBoxSize() const;
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Split;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Drop;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Consume;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<USlider> Slider_Split;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_SplitAmount;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<USizeBox> SizeBox_Root;
+
+	int32 GridIndex { INDEX_NONE };
+
+	UFUNCTION()
+	void SplitButtonClicked();
+
+	UFUNCTION()
+	void DropButtonClicked();
+
+	UFUNCTION()
+	void ConsumeButtonClicked();
+
+	UFUNCTION()
+	void SliderValueChanged(float NewValue);
+
+	int32 GetSplitAmount() const;
+
+public:
+	void SetGridIndex(const int32 InGridIndex) { GridIndex = InGridIndex; }
+	int32 GetGridIndex() const { return GridIndex; }
+};
