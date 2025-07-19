0f02274921efbb3a07c5ca4435f7a27bf9f88881
filliperomero <fillipe.romero@gmail.com>
2025-07-18 23:54:25 -0600

    Add ability to fill the stack when the amount exceeds the limit

--- Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -549,13 +549,21 @@ void UInv_InventoryGrid::OnSlottedItemClicked(int32 GridIndex, const FPointerEve
 		if (ShouldSwapStackCounts(RoomInClickedSlot, HoveredStackCount, MaxStackSize))
 		{
 			SwapStackCounts(ClickedStackCount, HoveredStackCount, GridIndex);
+			return;
 		}
 
 		if (ShouldConsumeHoverItemStacks(HoveredStackCount, RoomInClickedSlot))
 		{
 			ConsumeHoverItemStacks(ClickedStackCount, HoveredStackCount, GridIndex);
+			return;
 		}
-		// Should we fill in the stacks of the clicked item? (and not consume the hover item)
+
+		if (ShouldFillInStack(HoveredStackCount, RoomInClickedSlot))
+		{
+			FillInStack(RoomInClickedSlot, HoveredStackCount - RoomInClickedSlot, GridIndex);
+			return;
+		}
+		
 		// Is there no room in the clicked slot?
 		return;
 	}
@@ -808,6 +816,24 @@ void UInv_InventoryGrid::ConsumeHoverItemStacks(const int32 ClickedStackCount, c
 	const FInv_GridFragment* GridFragment = GridSlots[Index]->GetInventoryItem()->GetItemManifest().GetFragmentOfType<FInv_GridFragment>();
 	const FIntPoint Dimensions = GridFragment ? GridFragment->GetGridSize() : FIntPoint(1, 1);
 	HighlightSlots(Index, Dimensions);
+}
+
+bool UInv_InventoryGrid::ShouldFillInStack(const int32 HoveredStackCount, const int32 RoomInClickedSlot) const
+{
+	return RoomInClickedSlot < HoveredStackCount;
+}
+
+void UInv_InventoryGrid::FillInStack(const int32 FillAmount, const int32 Remainder, const int32 Index)
+{
+	UInv_GridSlot* GridSlot = GridSlots[Index];
+	const int32 NewStackCount = GridSlot->GetStackCount() + FillAmount;
+
+	GridSlot->SetStackCount(NewStackCount);
+
+	UInv_SlottedItem* ClickedSlottedItem = SlottedItems.FindChecked(Index);
+	ClickedSlottedItem->UpdateStackCount(NewStackCount);
+
+	HoverItem->UpdateStackCount(Remainder);
 }
 
 void UInv_InventoryGrid::ShowCursor()
