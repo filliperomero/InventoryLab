27fe4444705c2f28b750b189e734f615aaba133e
filliperomero <fillipe.romero@gmail.com>
2025-07-21 23:18:46 -0600

    Add Item Description Widget (basic functionality)

---
+++ Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/ItemDescription/Inv_ItemDescription.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Inv_ItemDescription.h"
+
+#include "Components/SizeBox.h"
+
+FVector2D UInv_ItemDescription::GetBoxSize()
+{
+	return SizeBox->GetDesiredSize();
+}
---
+++ Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/ItemDescription/Inv_ItemDescription.h
@@ -0,0 +1,25 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_ItemDescription.generated.h"
+
+class USizeBox;
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_ItemDescription : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	FVector2D GetBoxSize();
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USizeBox> SizeBox;
+};
