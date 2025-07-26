111e24c9ce85938d6fe8b3a4d05059efba34b84c
filliperomero <fillipe.romero@gmail.com>
2025-07-25 23:38:45 -0600

    Add Leaf Image, Modify ImageFragment to inherit from InventoryItemFragment, Add Leaf_Icon widget

---
+++ Plugins/InventorySystem/Source/InventorySystem/Private/Widgets/Composite/Inv_Leaf_Image.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Widgets/Composite/Inv_Leaf_Image.h"
+
+#include "Components/Image.h"
+#include "Components/SizeBox.h"
+
+void UInv_Leaf_Image::SetImage(UTexture2D* InTexture) const
+{
+	Image_Icon->SetBrushFromTexture(InTexture);
+}
+
+void UInv_Leaf_Image::SetBoxSize(const FVector2D& InSize) const
+{
+	SizeBox_Icon->SetWidthOverride(InSize.X);
+	SizeBox_Icon->SetHeightOverride(InSize.Y);
+}
+
+void UInv_Leaf_Image::SetImageSize(const FVector2D& InSize) const
+{
+	Image_Icon->SetDesiredSizeOverride(InSize);
+}
+
+FVector2D UInv_Leaf_Image::GetImageSize() const
+{
+	return Image_Icon->GetDesiredSize();
+}
---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/Widgets/Composite/Inv_Leaf_Image.h
@@ -0,0 +1,32 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inv_Leaf.h"
+#include "Inv_Leaf_Image.generated.h"
+
+class USizeBox;
+class UImage;
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_Leaf_Image : public UInv_Leaf
+{
+	GENERATED_BODY()
+
+public:
+	void SetImage(UTexture2D* InTexture) const;
+	void SetBoxSize(const FVector2D& InSize) const;
+	void SetImageSize(const FVector2D& InSize) const;
+	FVector2D GetImageSize() const;
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> Image_Icon;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<USizeBox> SizeBox_Icon;
+};
