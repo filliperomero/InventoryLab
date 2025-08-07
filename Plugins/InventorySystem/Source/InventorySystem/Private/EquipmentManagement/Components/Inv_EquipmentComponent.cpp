826fa7d3b956b846c49ec3a0b2f6569627ce44c3
filliperomero <fillipe.romero@gmail.com>
2025-08-06 23:19:46 -0600

    Add Equipment Component

---
+++ Plugins/InventorySystem/Source/InventorySystem/Private/EquipmentManagement/Components/Inv_EquipmentComponent.cpp
@@ -0,0 +1,52 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "EquipmentManagement/Components/Inv_EquipmentComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/Character.h"
+#include "InventoryManagement/Components/Inv_InventoryComponent.h"
+#include "InventoryManagement/Utils/Inv_InventoryStatics.h"
+
+UInv_EquipmentComponent::UInv_EquipmentComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UInv_EquipmentComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OwningPlayerController = Cast<APlayerController>(GetOwner());
+	if (OwningPlayerController.IsValid())
+	{
+		if (const ACharacter* OwningCharacter = Cast<ACharacter>(OwningPlayerController->GetCharacter()); IsValid(OwningCharacter))
+		{
+			OwningSkeletalMesh = OwningCharacter->GetMesh();
+		}
+
+		InitInventoryComponent();
+	}
+}
+
+void UInv_EquipmentComponent::InitInventoryComponent()
+{
+	InventoryComponent = UInv_InventoryStatics::GetInventoryComponent(OwningPlayerController.Get());
+	if (!InventoryComponent.IsValid()) return;
+
+	if (!InventoryComponent->OnItemEquipped.IsAlreadyBound(this, &ThisClass::OnItemEquipped))
+	{
+		InventoryComponent->OnItemEquipped.AddDynamic(this, &ThisClass::OnItemEquipped);
+	}
+
+	if (!InventoryComponent->OnItemUnequipped.IsAlreadyBound(this, &ThisClass::OnItemUnequipped))
+	{
+		InventoryComponent->OnItemUnequipped.AddDynamic(this, &ThisClass::OnItemUnequipped);
+	}
+}
+
+void UInv_EquipmentComponent::OnItemEquipped(UInv_InventoryItem* EquippedItem)
+{
+}
+
+void UInv_EquipmentComponent::OnItemUnequipped(UInv_InventoryItem* EquippedItem)
+{
+}
---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/EquipmentManagement/Components/Inv_EquipmentComponent.h
@@ -0,0 +1,37 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inv_EquipmentComponent.generated.h"
+
+class UInv_InventoryItem;
+class UInv_InventoryComponent;
+class APlayerController;
+class USkeletalMeshComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
+class INVENTORYSYSTEM_API UInv_EquipmentComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UInv_EquipmentComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	TWeakObjectPtr<UInv_InventoryComponent> InventoryComponent;
+	TWeakObjectPtr<APlayerController> OwningPlayerController;
+	TWeakObjectPtr<USkeletalMeshComponent> OwningSkeletalMesh;
+
+	UFUNCTION()
+	void OnItemEquipped(UInv_InventoryItem* EquippedItem);
+
+	UFUNCTION()
+	void OnItemUnequipped(UInv_InventoryItem* EquippedItem);
+
+	void InitInventoryComponent();
+};
